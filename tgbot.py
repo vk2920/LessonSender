aa696d69ca0c05e439984332a6f3e79926b45fd9
vk2920 <vk2920@yandex.ru>
2021-09-14 18:59:18 +0300

    ИТсправлено логирование и добавлены комментарии

--- tgbot.py
+++ tgbot.py
@@ -1,6 +1,5 @@
 import datetime
 import logging
-import time
 
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.utils.markdown import text, bold, italic, code, underline, strikethrough
@@ -20,16 +19,22 @@ psdb = PSDB()
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    with open("log.txt", "a") as f:
-        f.write("[" + str(datetime.datetime.today().strftime("%Y-%m-%d %H.%M.%S")) + "] " + \
-                str(message.from_user.id) + " - " + str(message.from_user.username) + " - " + \
-                str(message.from_user.first_name) + ": " + str(message.text) + "\n")
+    # Запись в логах лишней не будет
+    print("[" + str(datetime.datetime.today().strftime("%Y-%m-%d %H.%M.%S")) + "] " + \
+          str(message.from_user.id) + " - " + str(message.from_user.username) + " - " + \
+          str(message.from_user.first_name) + ": " + str(message.text) + "\n")
+
+    # Определим чётность недели и номера нужных дней недели
     even_week = int(datetime.date.today().strftime("%V")) % 2 == 0
     today = datetime.datetime.today().weekday()
     tomorrow = today+1 if today != 6 else 0
+
+    # Текст в начале сообщения
     msg = """Сейчас для меня нет разницы, что ты написал.\n""" + \
           """Но я составляю ответ на основе содержимого своей базы данных\n""" + \
           """Здесь есть только расписание для группы создателей (ИС/б-21-3-о)\n\n"""
+
+    # Добавим к ответу бота расписание на сегодня
     if today != 6: # Если сегодня не воскресенье
         msg += bold("Вот твоё расписание на сегодня:") + "\n"
         pairs = psdb.r_get_pairs_by_group(day_of_week=today+1, even_week=even_week, group="ИС/б-21-3-о")
@@ -53,6 +58,7 @@ async def echo(message: types.Message):
             #        str(pair[7]) + " в аудитории " + str(pair[8]) + ")\n"
             msg += str(pair[4]) + str(pair[5]) + "\n    " + italic(str(pair[6])) + "\n    " + code(str(pair[7]) + " в ауд. " + str(pair[8])) + "\n\n"
 
+    # Добавим в сообщение расписание на завтра
     if tomorrow != 6: # Если завтра не воскресенье
         msg += bold("Вот твоё расписание на завтра:") + "\n"
         pairs = psdb.r_get_pairs_by_group(day_of_week=tomorrow + 1, even_week=even_week, group="ИС/б-21-3-о")
@@ -77,6 +83,7 @@ async def echo(message: types.Message):
             msg += str(pair[4]) + str(pair[5]) + "\n    " + italic(str(pair[6])) + "\n    " + code(
                 str(pair[7]) + " в ауд. " + str(pair[8])) + "\n\n"
 
+    # Ассинхронный ответ на сообщение пользователя
     await message.answer(msg.replace('\\', ''), parse_mode=types.ParseMode.MARKDOWN)
 
 if __name__ == '__main__':
